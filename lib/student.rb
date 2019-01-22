582ca44beb0a674691deb7266886771ffdc73d34
Anuj Panchal <anujpan1@gmail.com>
2019-01-22 01:40:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,10 @@ require_relative "../config/environment.rb"
 
 class Student
   attr_accessor :name, :grade
+  attr_reader :id
   
   def initialize(id = nil, name, grade)
+    
     @name = name
     @grade = grade
   end
